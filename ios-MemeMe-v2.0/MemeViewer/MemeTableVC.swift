ae455396c07c79e381c39f9c7f0219538c6eca08
Abdullah-MGK <abdullah_mgk@outlook.com>
2020-05-30 02:53:20 +0300

    update

--- ios-MemeMe-v2.0/MemeViewer/MemeTableVC.swift
+++ ios-MemeMe-v2.0/MemeViewer/MemeTableVC.swift
@@ -123,14 +123,4 @@ class MemeTableVC: UITableViewController, MemeEditorDelegate {
         present(controller, animated: true, completion: nil)
     }
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
 }
