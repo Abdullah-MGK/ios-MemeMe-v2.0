cb68a14957a4340b7c0907c8e4f69bbff009ab4c
Abdullah-MGK <abdullah_mgk@outlook.com>
2020-06-03 09:06:59 +0300

    update

--- ios-MemeMe-v2.0/MemeViewer/MemeTableVC.swift
+++ ios-MemeMe-v2.0/MemeViewer/MemeTableVC.swift
@@ -9,12 +9,15 @@ import UIKit
 
 class MemeTableVC: UITableViewController, MemeEditorDelegate {
     
+    // stores identifiers
     let memeCellId = "MemeCell"
     let memeDetailId = "MemeDetailVC"
     
+    // stores table row height and spacing
     let rowHeight: CGFloat = 100.0
     let spacing: CGFloat = 15.0
     
+    // datasource
     var memes: [Meme]! {
         return (UIApplication.shared.delegate as! AppDelegate).memes
     }
@@ -27,17 +30,11 @@ class MemeTableVC: UITableViewController, MemeEditorDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // to make table separators edge-2-edge
+        // makes table separators edge-to-edge
         tableView.separatorInset = UIEdgeInsets.zero
         
-        // to hide / remove extra table rows
+        // hides/removes extra table rows
         tableView.tableFooterView = UIView()
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -57,44 +54,12 @@ class MemeTableVC: UITableViewController, MemeEditorDelegate {
     
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         
+        // makes the spacing to be transparant
         let view = UIView()
         view.backgroundColor = .clear
         
         return view
     }
-    
-    override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-        
-        return 1
-    }
-    
-    override func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-        
-        let view = UIView()
-        view.backgroundColor = .clear
-        
-        return view
-    }
-    
-    //    override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-    //
-    //        return 1
-    /*
-     //       return spacing
-     */
-    //    }
-    
-    //    override func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-    //
-    //        let v = UIView(frame: CGRect(x: 0, y:0, width: tableView.frame.width, height: 1))
-    //        v.backgroundColor = .darkGray
-    //         return v
-    /*
-     //        let view = UIView()
-     //        view.backgroundColor = .clear
-     //         return view
-     */
-    //    }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
@@ -102,9 +67,9 @@ class MemeTableVC: UITableViewController, MemeEditorDelegate {
         
         let meme = memes[indexPath.section]
         
+        // assigns meme details to cell properties
         cell.textLabel?.numberOfLines = 2
         cell.textLabel?.text = "\(meme.topTxt)\n\(meme.bottomTxt)"
-        // cell.detailTextLabel?.text = meme.bottomTxt
         cell.imageView?.image = meme.memedImg
         
         return cell
@@ -112,6 +77,7 @@ class MemeTableVC: UITableViewController, MemeEditorDelegate {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
+        // shows memeDetailController with the selected meme
         let detailController = storyboard!.instantiateViewController(withIdentifier: memeDetailId) as! MemeDetailVC
         
         detailController.meme = memes[indexPath.section]
@@ -126,11 +92,12 @@ class MemeTableVC: UITableViewController, MemeEditorDelegate {
     
     override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         
+        // adds swipe left to delete action
         let deleteAction = UIContextualAction(style: .destructive, title: "Delete", handler: { (deleteAction, view, completion) in
             
+            // updates datasource & table
             (UIApplication.shared.delegate as! AppDelegate).memes.remove(at: indexPath.section)
             tableView.deleteSections([indexPath.section], with: .automatic)
-            // self.updateView()
             
         })
         
@@ -141,10 +108,12 @@ class MemeTableVC: UITableViewController, MemeEditorDelegate {
     
     override func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         
+        // adds swipe right to share action
         let shareAction = UIContextualAction(style: .normal, title: "Share", handler: { (shareAction, view, completion) in
                         
             let image = self.memes[indexPath.section].memedImg
             
+            // shows share actvityViewController
             let controller = UIActivityViewController(activityItems: [image], applicationActivities: nil)
             
             self.present(controller, animated: true, completion: nil)
@@ -156,24 +125,9 @@ class MemeTableVC: UITableViewController, MemeEditorDelegate {
         return swipeActions
     }
     
-    /*
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     return true
-     }
-     
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-     
-     if editingStyle == .delete {
-     (UIApplication.shared.delegate as! AppDelegate).memes.remove(at: indexPath.row)
-     updateView()
-     }
-     
-     }
-     */
-    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
-        // assign self as delgate for MemeEditorVC in order to update the tableView when MemeEditorVC is dismissed
+        // assigns self as delgate for MemeEditorVC in order to update the tableView when MemeEditorVC is dismissed
         let memeEditorController = (segue.destination as! UINavigationController).topViewController as! MemeEditorVC
         
         memeEditorController.delegate = self
